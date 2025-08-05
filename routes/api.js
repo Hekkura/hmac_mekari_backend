5945bc3b4f3ce13cab212042e65115d17aaa8b5c
prog8melawai <prog8melawai@gmail.com>
2025-08-05 17:38:12 +0700

    route profile API

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,10 @@ const getController = require('../controllers/mekariGetController.js')
 const postController = require('../controllers/mekariPostController.js')
 const profileController = require('../controllers/profileController.js')
 
-router.get('/profile', profileController.getMekariProfile)
+router.get('/Mekariprofile', profileController.getMekariProfile)
+router.get('/getProfile', profileController.getProfileData) //change names
+router.post('/postProfile',profileController.postProfileData) //change names
+router.delete('delProfile', profileController.deleteProfileData) //change names
 
 router.get('/documents', getController.getDocumentLists)
 router.get('/documents/:documentId', getController.getDocumentDetail)
